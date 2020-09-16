09e87dbe23b0d826b6b027f3b703388727d91566
nhjoy <nhjoy7232@gmail.com>
2020-09-16 18:40:25 +0800

    __main__ == main code  and sys.argv to access cmd

--- disk_report.py
+++ disk_report.py
@@ -1,1 +1,14 @@
 #!usr/bin/python3
+
+#import library
+import sys
+
+#check if standalone program
+if __name__ == "__main__":
+    path ='/home'
+    print ("total arguments passed: ", len(sys.argv))
+
+    if len(sys.argv) >= 2:
+        directory = sys.argv[1]
+    else:
+        directory = path
