1725a994ad45f4add8ea1cafaae924e0700689bc
nhjoy <nhjoy7232@gmail.com>
2020-09-16 19:10:05 +0800

    added codes to access sud_dir and files under home

--- disk_report.py
+++ disk_report.py
@@ -2,6 +2,7 @@
 
 #import library
 import sys
+import os
 
 #check if standalone program
 if __name__ == "__main__":
@@ -12,3 +13,10 @@ if __name__ == "__main__":
         directory = sys.argv[1]
     else:
         directory = path
+
+    #loop to access sub-directories and files
+    for entry in os.scandir(directory):
+        print(entry.path)
+        #checks entry is directory and make sure not symbolic link
+        if (entry.is_dir(follow_symlinks=False)):
+            print(entry.path + " is a directory")
