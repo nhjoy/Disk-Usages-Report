8887813f727ae21f31788e63e718cd34e76d3583
nhjoy <nhjoy7232@gmail.com>
2020-09-16 20:17:27 +0800

    pandas to generate csv report

--- disk_report.py
+++ disk_report.py
@@ -3,6 +3,7 @@
 #import library
 import sys
 import os
+import pandas as pd
 
 #function- calculate disk space by each dir
 def get_size(path):
@@ -28,11 +29,27 @@ if __name__ == "__main__":
         directory = sys.argv[1]
     else:
         directory = path
+    
+    #list created
+    usage = []
+    paths = []
 
     #loop to access sub-directories and files
     for entry in os.scandir(directory):
         print(entry.path)
         #checks entry is directory and make sure not symbolic link
         if (entry.is_dir(follow_symlinks=False)):
-            print(entry.path + " is a directory")
-            print(get_size(entry.path))
+            # print(entry.path + " is a directory")
+            # print(get_size(entry.path))
+            
+            #appending the value of total and path in the lists
+            total = get_size(entry.path)
+            print(total)
+            paths.append(entry.path)
+            usage.append(total)
+        
+        #dictionary created
+        usage_dict = {'directory' : paths, 'usage' : usage}
+        df = pd.DataFrame(usage_dict)
+        print(df)
+        df.to_csv("disk_usages.csv")
